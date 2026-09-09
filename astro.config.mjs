0ae7551889acffccafc13f8979a6d7937df0b33f
Yousef AlZayat <67283820+YousefAlzayat@users.noreply.github.com>
2026-09-09 17:14:47 +0300

    further updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://YAlZayat.com',
-  base: '/', // Set to '/' if your GitHub repository is named YousefAlzayat.github.io
+  site: 'https://yalzayat.com',
+  base: '/',
   vite: {
     plugins: [tailwindcss()],
   },
